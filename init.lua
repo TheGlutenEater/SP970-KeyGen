19240e1952bca04ea587c2a4fde12bdf205509be
hsdbql <hsdbql@163.com>
2022-05-21 22:26:40 +0800

    1.2

---
+++ init.lua
@@ -0,0 +1,15 @@
+appname="SP970 keyGen"
+appver="1.0"
+appcode="1"
+appsdk="15"
+path_pattern=""
+packagename="org.nmmsl.sp970.keygen"
+theme="Theme_Holo"
+app_key=""
+app_channel=""
+developer=""
+description=""
+debugmode=false
+user_permission={
+  
+}
